c72f57b8f2c8290d1de42601c103e6fea986bf99
Kolibri <dev@kolibri.mx>
2021-03-23 17:50:06 -0600

    Add Full Script

--- Lambda/Lambdas.sh
+++ Lambda/Lambdas.sh
@@ -1,21 +1,66 @@
 #!/usr/bin/env bash
 
-FUNCTION_NAME="sd_calendartournamentsactive-test"
-DESCRIPTION_OO="Lambda for obtain active tournaments from Opta"
-ROLE_00="SportData-CalendarTournamentsActive-Role"
+# Services lists
+
+# Lambdas
+
+lambdas=(
+    sd_calendartournamentsactive-test
+    sd_calendarmatchbyyear-test
+    sd_calendarnormalize-test
+    sd_calendartournamentsbymonth-test
+    sd_calendarstorage-test
+    sd_calendargetcalendarbymonth-test
+)
+
+# Roles
+
+roles=(
+    SportData-CalendarTournamentsActive-Role
+    SportData-CalendarMatchByYear-Role			
+    SportData-CalendarNormalize-Role					
+    SportData-CalendarTournamentsByMonth-Role
+    SportData-CalendarStorage-Role		
+    SportData-CalendarGetCalendarByMonth-Role
+)
+
+# Descriptions
+
+descriptions=(
+    "Lambda for obtain active tournaments from Opta"
+    "Lambda to get matches by year"		
+    "Lambda for normalize calendar data"					
+    "Lambda to get calendar data by month"
+    "Lambda to get data from S3 Bucket"		
+    "Lambda to get Api Gateway endpoints"
+)
+
+# Base configurations
 
 RUNTIME="nodejs14.x"
 BUCKET="devops-cloudprojects"
 BUCKET_DIR="Lambda-Placeholder/index.js.zip"
 ACCOUNT_ID="528662748254"
 
-aws lambda create-function \
-    --function-name $FUNCTION_NAME \
+# AWS CLI 2 Lambda Create Function
+
+for lambda_services in ${!lambdas[@]}; do
+    aws lambda create-function \
+    --function-name ${lambdas[$lambda_services]} \
     --runtime $RUNTIME \
     --handler index.handler \
     --code S3Bucket=$BUCKET,S3Key=$BUCKET_DIR \
-    --role arn:aws:iam::${ACCOUNT_ID}:role/$ROLE_00 \
-    --description "${DESCRIPTION_00}" \
-    --tags Area=DevOps,Branch=TUDN,CodeName='Third Party',Environment=All,Project=Sport-Data,Service=Calendar
+    --role arn:aws:iam::${ACCOUNT_ID}:role/${roles[$lambda_services]} \
+    --description "${descriptions[$lambda_services]}" \
+    --tags Area=DevOps,Branch=TUDN,CodeName='Third Party',Environment=All,Project=Sport-Data,Service=Calendar \
+    --output yaml >> Lambdas.txt
+done
 
+# Echo test:
 
+# for lambda_services in ${!lambdas[@]}; do
+#     echo ${lambdas[$lambda_services]}
+#     echo ${roles[$lambda_services]}
+#     echo ${descriptions[$lambda_services]}
+#     echo ""
+# done

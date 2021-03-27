7eca5ea5420a146255132f55e397d50ea7f4f3de
Kolibri <dev@kolibri.mx>
2021-03-26 20:03:12 -0600

    Add Array policies creation resolve #10

--- Policies/Policies-Short.sh
+++ Policies/Policies-Short.sh
@@ -1,26 +1,47 @@
 #!/usr/bin/env bash
 
-POLICY_NAME="my-policy"
-DESCRIPTION="My description"
+# Services List
 
 # Policies
 
-aws iam create-policy \
---policy-name $POLICY_NAME \
---description "$DESCRIPTION" \
---tags Key=Author,Value=Kolibri Key=Project,Value='Kolibri AWS' \
---policy-document \
-'{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                " : ",
-                " : "
-            ],
-            "Resource": "arn:aws: ... "
-        }
-    ]
-}' \
---output yaml >> Policies-Short.yml
+policies=(
+    # Policies List
+)
+
+# Descriptions
+
+descriptions=(
+    # Descriptions List
+)
+
+AWS CLI 2 Policy Create Function
+
+for policy_rules in ${!policies[@]}; do
+    aws iam create-policy \
+    --policy-name ${policies[$policy_rules]} \
+    --description "${descriptions[$policy_rules]}" \
+    --tags Key=Author,Value=Kolibri Key=Project,Value='Kolibri AWS' \
+    --policy-document \
+    '{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    " : ",
+                    " : "
+                ],
+                "Resource": "arn:aws: ... "
+            }
+        ]
+    }' \
+    --output yaml >> Policies-Short.yml
+done
+
+# Echo test:
+
+# for policy_rules in ${!policies[@]}; do
+#     echo ${policies[$policy_rules]}
+#     echo ${descriptions[$policy_rules]}
+#     echo ""
+# done
